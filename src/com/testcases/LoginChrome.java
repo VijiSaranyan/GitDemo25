0ebe9740808d9af7a29b029848ec455aebde9324
Viji <vandhuraghavan@gmail.com>
2020-11-05 16:20:15 -0800

    This is commit from   Asian time zone

--- src/com/testcases/LoginChrome.java
+++ src/com/testcases/LoginChrome.java
@@ -8,6 +8,7 @@ public class LoginChrome {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Viji\\Desktop\\Selenium\\chromedriver.exe");
 		WebDriver  driver=new ChromeDriver();
+		System.out.println("The chrome browser is opened");
 	}
 
 }
